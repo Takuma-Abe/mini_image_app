9e7947e473451bb7509fc379244f10c3ae5e0044
Bee-bird <64572909+Takuma-Abe@users.noreply.github.com>
2020-09-15 15:44:28 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,14 @@ class MessagesController < ApplicationController
 
   def index
     @messages = Message.all
-  end
+  
 
   def new
     @message = Message.new
   end
 
   def create
-    @messaga = Message.new(message_params)
+    @message = Message.new(message_params)
     if @message.save
       redirect_to root_path
     else
